b14c2fdc1127c958527e3e7ee7559f747262aa61
Alessandro Tegner <alessandro.tegner@gmail.com>
2024-09-22 20:41:32 -0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
+
+type DNSMessage struct {
+	ID      uint16 // transaction ID
+	QDCount uint16 // number of entries in the question section
+	AnCount uint16 // number of records in the answer section
+	NsCount uint16 // number of records in the authority section
+	ArCount uint16 // number of records in the additional section
+
+	Opcode uint8 // 4 bits
+	Z      uint8 // 3 bits reserved for future use by DNSSEC
+	Rcode  uint8 // 4 bits response code
+
+	QR bool // indicates whether the message is a query (0) or a reply (1)
+	AA bool // Authoritative Answer
+	TC bool // Truncation
+	RD bool // Recursion Desired
+	RA bool // Recursion Available
+}
+
+func (m *DNSMessage) Marshal() []byte {
+	marshaled := make([]byte, 12)
+	binary.BigEndian.PutUint16(marshaled[0:2], m.ID)
+	marshaled[2] = 1 << 7 // QR
+
+	return marshaled
+
+}
 
 // Ensures gofmt doesn't remove the "net" import in stage 1 (feel free to remove this!)
 var _ = net.ListenUDP
@@ -27,22 +55,32 @@ func main() {
 	}
 	defer udpConn.Close()
 
-	buf := make([]byte, 512)
+	req := make([]byte, 512)
 
 	for {
-		size, source, err := udpConn.ReadFromUDP(buf)
+		_, client, err := udpConn.ReadFromUDP(req)
 		if err != nil {
 			fmt.Println("Error receiving data:", err)
 			break
 		}
 
-		receivedData := string(buf[:size])
-		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+		reply := DNSMessage{
+			ID:      1234,
+			QR:      true,
+			Opcode:  0,
+			AA:      false,
+			TC:      false,
+			RD:      true,
+			RA:      false,
+			Z:       0,
+			Rcode:   0,
+			QDCount: 0,
+			AnCount: 0,
+			NsCount: 0,
+			ArCount: 0,
+		}
 
-		// Create an empty response
-		response := []byte{}
-
-		_, err = udpConn.WriteToUDP(response, source)
+		_, err = udpConn.WriteToUDP(reply.Marshal(), client)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
